43f9aeb3f4513c20cb0be523c41c779bacfa1563
Luca Colomba <lc.colomba@gmail.com>
2022-07-14 01:26:52 +0200

    added metrics logging

--- models/lit_rnn.py
+++ models/lit_rnn.py
@@ -2,6 +2,7 @@ import torch
 import pytorch_lightning as pl
 
 from torch.utils.data import DataLoader
+from torchmetrics import Accuracy, Precision, Recall, F1Score
 
 from typing import Union, Dict, List, Tuple
 
@@ -16,6 +17,13 @@ class LitRNN(pl.LightningModule):
         self.loss = loss
         self.optimizer_klass = optimizer_klass
         self.optimizer_args = optimizer_args
+
+        self.metrics = [
+            Accuracy(num_classes=self.model.nclasses, average='micro', multiclass=True),
+            Precision(num_classes=self.model.nclasses, average='micro', multiclass=True),
+            Recall(num_classes=self.model.nclasses, average='micro', multiclass=True),
+            F1Score(num_classes=self.model.nclasses, average='micro', multiclass=True)
+        ]
 
         return
 
@@ -32,7 +40,16 @@ class LitRNN(pl.LightningModule):
         out = self.model(x)
         loss = self.loss(out, y)
 
+        for m in self.metrics:
+            m.update(out, y)
+
         return loss
+
+    def training_epoch_end(self, outputs) -> None:
+        for m in self.metrics:
+            val = m.compute()
+            print(f'{type(m).__name__}: {val}')
+        return
 
     def validation_step(self, validation_batch, batch_idx):
         x, y = validation_batch
@@ -41,6 +58,15 @@ class LitRNN(pl.LightningModule):
 
         if batch_idx % 10 == 0:
             print(f'{batch_idx}) Validation loss: {loss}')
+
+        for m in self.metrics:
+            m.update(out, y)
+        return
+
+    def validation_epoch_end(self, outputs) -> None:
+        for m in self.metrics:
+            val = m.compute()
+            print(f'{type(m).__name__}: {val}')
         return
 
     def backward(self, loss, optimizer, optimizer_idx) -> None:
